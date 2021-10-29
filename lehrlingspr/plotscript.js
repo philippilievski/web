2aa256d071471b2ba51d0ddcadd5ff1c6632b22f
Philipp Ilievski <Philipp.ILIEVSKI@bachmann.info>
2021-10-29 13:31:11 +0200

    init

---
+++ lehrlingspr/plotscript.js
@@ -0,0 +1,22 @@
+var data = [
+    {
+      x: ['Workshop', 'APOO-AT', 'TF-TE', 'IT-Technik', 'AU_SE', 'TR-Runtime', 'TR-Communication'],
+      y: [1, 1.75, 1, 1.75, 4.5, 6, 15],
+      marker:{
+        color: '#D4ECDD'
+      },
+      type: 'bar'
+    }
+  ];
+
+var layout = {
+    plot_bgcolor: "#152D35",
+    paper_bgcolor: "#152D35",
+    font: {
+        family: 'Courier New, monospace',
+        size: 18,
+        color: "#D4ECDD"
+      }
+}
+  
+  Plotly.newPlot('abteilungenplot', data, layout);
