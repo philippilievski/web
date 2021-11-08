a257065802fd26992c942c274a56df419b264152
Philipp Ilievski <Philipp.ILIEVSKI@bachmann.info>
2021-11-08 13:20:00 +0100

    added responsive design

--- lehrlingspr/scrollscript.js
+++ lehrlingspr/scrollscript.js
@@ -1,5 +1,4 @@
-const scroller = new LocomotiveScroll({
-    el: document.querySelector("[data-scroll-container]"),
-    smooth: true,
-    smartphone: { smooth: true }
-  });
+var scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
+});
